0ac6059a23d40eb036e9014f3b469dca9e426ab5
chiefoleka <valentinenwakibeya@gmail.com>
2017-12-04 11:35:34 +0100

    Hide login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                         <!-- Authentication Links -->
                         <li><a href="{{ route('shareincident') }}"><strong><i class="fa fa-plus"></i> Share your story</strong></a></li>
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
+                           <!--  <li><a href="{{ route('login') }}">Login</a></li> -->
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
