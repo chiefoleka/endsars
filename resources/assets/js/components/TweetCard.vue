1c658ee2cc9b99bdfed859d16006fc432a42cc9a
chiefoleka <valentinenwakibeya@gmail.com>
2017-12-10 09:43:43 +0100

    Removed retweet count on twitter stories

--- resources/assets/js/components/TweetCard.vue
+++ resources/assets/js/components/TweetCard.vue
@@ -2,7 +2,6 @@
     <div class="col-md-4 tweet" style="margin-top: 40px">
         <a :href="'https://twitter.com/'+tweet.handle"><h4>{{tweet.name}}</h4></a>
         <div class="row summary" v-html="tweet.tweet"></div>
-        <span><i class="fa fa-retweet"></i> {{tweet.retweets}} &nbsp;</span>
         <br>
     </div>
 </template>
